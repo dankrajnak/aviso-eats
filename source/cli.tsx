c9c13743d455425c45b8ab056bdece868cf563ae
Daniel Krajnak <dan.krajnak@gmail.com>
2021-10-02 14:54:15 -0400

    Initial commit

---
+++ source/cli.tsx
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './ui';
+
+const cli = meow(`
+	Usage
+	  $ aviso-eats
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ aviso-eats --name=Jane
+	  Hello, Jane
+`, {
+	flags: {
+		name: {
+			type: 'string'
+		}
+	}
+});
+
+render(<App name={cli.flags.name}/>);
